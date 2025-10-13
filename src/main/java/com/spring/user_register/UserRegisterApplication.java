1771e6e1f2a2d2f5a4eeeaecac7e7423269a8cac
EricdSouzx <eericdsn@gmail.com>
2025-10-13 19:38:47 -0300

    chore: commit initial

---
+++ src/main/java/com/spring/user_register/UserRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.spring.user_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegisterApplication.class, args);
+	}
+
+}
