a2b12efb3987532f1bbf87a3c0a8a9451c789458
EricdSouzx <eericdsn@gmail.com>
2025-10-21 20:30:39 -0300

    feat(auth): add user role claim to JWT token generation

--- src/main/java/com/spring/user_register/security/TokenService.java
+++ src/main/java/com/spring/user_register/security/TokenService.java
@@ -22,8 +22,9 @@ public class TokenService {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
             String token = JWT.create()
-                    .withIssuer("login-auth-api")
+                    .withIssuer("ms-user-register")
                     .withSubject(user.getEmail())
+                    .withClaim("role", user.getRole().name())
                     .withExpiresAt(this.generateExpirationDate())
                     .sign(algorithm);
             return token;
@@ -36,7 +37,7 @@ public class TokenService {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.require(algorithm)
-                    .withIssuer("login-auth-api")
+                    .withIssuer("ms-user-register")
                     .build()
                     .verify(token )
                     .getSubject();
